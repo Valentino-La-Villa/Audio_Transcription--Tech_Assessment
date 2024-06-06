8e31d3495d0b5101674723c6d961998ebc94b40a
Valentino <duamnlavilla@gmail.com>
2024-06-06 19:27:07 -0300

    Added a little animation for when the user hovers any of the messages

--- src/components/transcription/Message.tsx
+++ src/components/transcription/Message.tsx
@@ -12,20 +12,24 @@ export default function Message(props: PropsType) {
         <div className={`d-flex col-11 col-sm-7
 
 
-        ${props.message.role === 'user' ? 'align-self-end justify-content-end' : 'align-self-start justify-content-start'}`} // Aligning user emited messages to the right, and agent emited messages to the left
+        ${props.message.role === 'user' 
+            ? 'align-self-end justify-content-end' 
+            : 'align-self-start justify-content-start'}`} // Aligning user emited messages to the right, and agent emited messages to the left
         >
 
             <p
             role="button"
-            onClick={()=>props.handleMessageClick(props.message.start)} 
-            style={{transition: 'all 1s'}}
+            onClick={()=>props.handleMessageClick(props.message.start)}
             className={`p-3 fs-6
 
+            paragraphAnimations
 
             ${(props.message.start < props.currentTimeReference && props.currentTimeReference < props.message.end)
-                ? 'opacity-100 border'
-                : 'opacity-75 border-transparent'}
-            ${props.message.role === 'user' ? 'bg-primary' : 'bg-danger'}`} // Setting a different color for messages made by the user and the agent
+                ? 'opacity-100 border border-white'
+                : 'opacity-75 border border-black'}
+            ${props.message.role === 'user' 
+                ? 'bg-primary' 
+                : 'bg-danger'}`} // Setting a different color for messages made by the user and the agent
             >
                 {props.message.content}
             </p>
