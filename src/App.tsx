c20109613312b9b81b8f6d70ba2db183eeec8c03
Valentino <duamnlavilla@gmail.com>
2024-06-06 19:05:37 -0300

    Fixed a bug that would cause the audio player to not update when the user clicked a message, due to the absence of useState

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,24 @@ import TranscriptionSection from './components/transcription/Transcription.tsx'
 import transcription from './data/transcription'
 import { MessageInformationObj } from './types/MessageInformationObj.ts'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const audioPlayer = document.getElementById('callAudio') as HTMLAudioElement
+  const [currentTimeReference, setCurrentTimeReference] = useState(0)
+
+  useEffect(()=>{ // This will update CurrentTimeReference in order to keep up with the user's input
+    const audioPlayer = document.getElementById('callAudio') as HTMLAudioElement
+
+    audioPlayer.addEventListener('timeupdate', ()=>{
+      setCurrentTimeReference(audioPlayer.currentTime)
+    })
+  }, [])
   
   function handleMessageClick(timeReference: number): void {
+    const audioPlayer = document.getElementById('callAudio') as HTMLAudioElement
     audioPlayer.currentTime = timeReference
+    audioPlayer.play()
   }
 
   return (
